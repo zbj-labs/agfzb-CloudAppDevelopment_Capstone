45314d30cbdc23bb3db27362494d394fb5799b85
zambujo <zambuj@gmail.com>
2022-06-07 22:49:57 +0200

    allow incomplete reviews

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -62,14 +62,18 @@ class CarDealer:
 # class `DealerReview` to hold review data
 class DealerReview:
     
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+    def __init__(self, dealership, name, purchase, review):
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
         self.review = review
-        self.purchase_date = purchase_date
-        self.car_make = car_make
-        self.car_model = car_model
-        self.car_year = car_year
-        self.sentiment = sentiment
-        self.id = id
+        # Optional fields
+        self.purchase_date = ""
+        self.car_make = ""
+        self.car_model = ""
+        self.car_year = ""
+        self.sentiment = ""
+        self.id = ""
+
+    def __str__(self):
+        return "Review: " + self.review
